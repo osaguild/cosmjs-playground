f22b77347a1c6159243393663789a1d406f04652
osaguild <nobuhisa.abe.0922@gmail.com>
2023-05-09 12:40:51 +0900

    add tx execution steps

--- experiment.ts
+++ experiment.ts
@@ -1,6 +1,15 @@
-import { IndexedTx, StargateClient } from "@cosmjs/stargate";
+import {
+  IndexedTx,
+  SigningStargateClient,
+  StargateClient,
+} from "@cosmjs/stargate";
+import {
+  DirectSecp256k1HdWallet,
+  OfflineDirectSigner,
+} from "@cosmjs/proto-signing";
 import { Tx } from "cosmjs-types/cosmos/tx/v1beta1/tx";
 import { MsgSend } from "cosmjs-types/cosmos/bank/v1beta1/tx";
+import { readFile } from "fs/promises";
 
 const rpc = "rpc.sentry-01.theta-testnet.polypore.xyz:26657";
 
@@ -34,6 +43,49 @@ const runAll = async (): Promise<void> => {
 
   const faucet: string = sendMessage.fromAddress;
   console.log("Faucet balances:", await client.getAllBalances(faucet));
+
+  const aliceSigner: OfflineDirectSigner = await getAliceSignerFromMnemonic();
+  const alice = (await aliceSigner.getAccounts())[0].address;
+  console.log("Alice's address from signer", alice);
+
+  const signingClient = await SigningStargateClient.connectWithSigner(
+    rpc,
+    aliceSigner
+  );
+  console.log(
+    "With signing client, chain id:",
+    await signingClient.getChainId(),
+    ", height:",
+    await signingClient.getHeight()
+  );
+
+  // Check the balance of Alice and the Faucet
+  console.log("Alice balance before:", await client.getAllBalances(alice));
+  console.log("Faucet balance before:", await client.getAllBalances(faucet));
+  // Execute the sendTokens Tx and store the result
+  const result = await signingClient.sendTokens(
+    alice,
+    faucet,
+    [{ denom: "uatom", amount: "100000" }],
+    {
+      amount: [{ denom: "uatom", amount: "500" }],
+      gas: "200000",
+    }
+  );
+  // Output the result of the Tx
+  console.log("Transfer result:", result);
+
+  console.log("Alice balance after:", await client.getAllBalances(alice));
+  console.log("Faucet balance after:", await client.getAllBalances(faucet));
+};
+
+const getAliceSignerFromMnemonic = async (): Promise<OfflineDirectSigner> => {
+  return DirectSecp256k1HdWallet.fromMnemonic(
+    (await readFile("./testnet.alice.mnemonic.key")).toString(),
+    {
+      prefix: "cosmos",
+    }
+  );
 };
 
 runAll();

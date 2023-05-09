0576fd0764695d6e5e233f014f10e022525cd97e
osaguild <nobuhisa.abe.0922@gmail.com>
2023-05-09 23:08:10 +0900

    fix url

--- cosmjs-keplr/app/page.tsx
+++ cosmjs-keplr/app/page.tsx
@@ -5,7 +5,7 @@ const Page: NextPage = () => {
   return (
     <FaucetSender
       faucetAddress="cosmos15aptdqmm7ddgtcrjvc5hs988rlrkze40l4q0he"
-      rpcUrl="rpc.sentry-01.theta-testnet.polypore.xyz"
+      rpcUrl="https://rpc.sentry-01.theta-testnet.polypore.xyz"
     />
   );
 };
